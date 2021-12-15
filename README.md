981ab0688a0f65f1c747780181b8b91d84ba8da2
MetaMan13 <imamovica1397@gmail.com>
2021-12-15 05:43:47 +0100

    Basic scroller done

---
+++ README.md
@@ -0,0 +1,6 @@
+# Python 9gag scroller
+
+## Setup
+
+    - Create .env file and enter your 9gag EMAIL & PASSWORD
+    - Start the script
--- scroller.py
+++ scroller.py
@@ -1,8 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from dotenv import load_dotenv
+from termcolor import colored
 import time
 import os
+
+
+def enterTopic():
+    topic = input('Please enter a 9gag topic: ')
+    topic = urls.get(topic)
+    return topic
+
 
 # Driver setup
 driver_options = webdriver.ChromeOptions()
@@ -10,9 +18,35 @@ driver_options.add_argument('--ignore-certificate-errors')
 driver_options.add_argument('--ignore-certificate-errors-spki-list')
 driver_options.add_argument('--ignore-ssl-errors')
 
-# Accept website url
-# url = input('Enter website link: ')
-url = 'https://9gag.com/funny'
+urls = {
+    'funny': 'https://9gag.com/funny',
+    'funny-fresh': 'https://9gag.com/funny/fresh',
+    'nsfw': 'https://9gag.com/nsfw',
+    'nsfw-fresh': 'https://9gag.com/nsfw',
+    'girl': 'https://9gag.com/girl',
+    'girl-fresh': 'https://9gag.com/girl/fresh',
+    'wtf': 'https://9gag.com/wtf',
+    'wtf-fresh': 'https://9gag.com/wtf/fresh',
+    'cryptocurrency': 'https://9gag.com/cryptocurrency',
+    'cryptocurrency-fresh': 'https://9gag.com/cryptocurrency/fresh',
+    'anime-and-manga': 'https://9gag.com/anime-manga/fresh',
+    'anime-and-manga-fresh': 'https://9gag.com/anime-manga/fresh',
+    'random': 'https://9gag.com/random',
+    'random-fresh': 'https://9gag.com/random/fresh',
+    'animals': 'https://9gag.com/animals',
+    'animals-fresh': 'https://9gag.com/animals/fresh',
+}
+
+print('\n' + colored('Available 9gag topics:',
+      'green', None, ['dark', 'underline']) + '\n')
+for i, url in enumerate(urls):
+    print(colored('[' + url + '] => ', 'yellow') + urls.get(url))
+
+url = enterTopic()
+
+while(url == None):
+    print(colored('Sorry that topic is not available, please choose again!', 'red'))
+    url = enterTopic()
 
 # Load enviorment variables
 load_dotenv()
